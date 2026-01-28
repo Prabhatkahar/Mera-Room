2ce16c41ac98422e3d133ef0c6d5c850953f1394
Prabhat <kahar0968@gmail.com>
2026-01-28 19:19:06 +0530

    h

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 ﻿import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./app/routes";
-import Providers from "./app/providers";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Providers>
-      <RouterProvider router={router} />
-    </Providers>
+    <App />
   </React.StrictMode>,
 );
